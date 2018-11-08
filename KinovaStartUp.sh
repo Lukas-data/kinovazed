d39e64bbd2e00e6b9335638c91ebb1956149deca
Raphael Schroeder <raphael.schroeder@hsr.ch>
2018-11-08 20:09:43 +0100

    StateMachine implemented, but not yet used in KinovaMain.

--- KinovaStartUp.sh
+++ KinovaStartUp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-until mate-terminal -x bash -c 'sudo /home/kinova/Documents/kinovaZED/build/KinovaZED -r;'; do
+until mate-terminal -x bash -c '/home/kinova/Documents/kinovaZED/_build/src -r;'; do
 
   echo "KinovaZED crashed with exit code $?. Restarting Script" >&2
   sleep 2
+
 done
 
