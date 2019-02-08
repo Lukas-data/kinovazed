5d9050a79b9ceee9075a7ea0f69fec9442e908d9
Raphael Schroeder <raphael.schroeder@hsr.ch>
2019-02-08 11:53:57 +0100

    Swaped to VisualStudioCode

--- _build/src/CMakeFiles/kinovaArm_lib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kinovaArm_lib.
-# This may be replaced when dependencies are built.
---
+++ build/test/CMakeFiles/logtest_lib.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kinova/Documents/kinovaZED/test/LogTestSubClass.cpp" "/home/kinova/Documents/kinovaZED/build/test/CMakeFiles/logtest_lib.dir/LogTestSubClass.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
