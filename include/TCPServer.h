042c02c1e144c36f1c6b2394cc2d4a53d343fd0f
Thomas Corbat <tcorbat@hsr.ch>
2019-07-22 13:59:07 +0200

    Format TCPServer

--- include/TCPServer.h
+++ include/TCPServer.h
@@ -3,47 +3,43 @@
 
 #include <netinet/in.h>
 
-
 #define BUFFER_SIZE 256
 #define COMMAND_LENGTH 6
 #define DATA_LENGTH 6
 #define DATA_PACKAGES 4
 #define NOCONNECTION_COUNT 20
 
-
 class TCPServer {
 
-	public:
-	  TCPServer() :
-      MessageLength(COMMAND_LENGTH + (DATA_PACKAGES*DATA_LENGTH)),
-      NoDataCycleCount(NOCONNECTION_COUNT),
-      clilen(0)
-      {}      
+public:
+	TCPServer() :
+			MessageLength(COMMAND_LENGTH + (DATA_PACKAGES * DATA_LENGTH)), NoDataCycleCount(NOCONNECTION_COUNT), clilen(0) {
+	}
 
-    bool connect();
+	bool connect();
 
-    int  getCommand();
-    int  getData(int dataPackage); //dataPackage = number of the requested Package (0-DATA_PACKAGES-1).
-    int  closeOnCommand(int command);
-    bool readTCP();
-    bool sendTCP(int command, int eventVar, int data1, int data2, int data3);
+	int getCommand();
+	int getData(int dataPackage); //dataPackage = number of the requested Package (0-DATA_PACKAGES-1).
+	int closeOnCommand(int command);
+	bool readTCP();
+	bool sendTCP(int command, int eventVar, int data1, int data2, int data3);
 
-  private:
+private:
 
-    void error(const char *funcName, const char *msg);
+	void error(const char *funcName, const char *msg);
 
-    int  sockfd, newsockfd, portno, clilen;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in serv_addr, cli_addr;
-    int  n;
+	int sockfd, newsockfd, portno, clilen;
+	char buffer[BUFFER_SIZE];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
 
-    int  commandRecieved;
-    int  commandToSend;
-    int  dataRecieved[DATA_PACKAGES];
-    int  dataToSend[DATA_PACKAGES];
+	int commandRecieved;
+	int commandToSend;
+	int dataRecieved[DATA_PACKAGES];
+	int dataToSend[DATA_PACKAGES];
 
-    int  MessageLength;
-    int  NoDataCycleCount;
-    
+	int MessageLength;
+	int NoDataCycleCount;
+
 };
 #endif
--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -12,141 +12,129 @@
 #include <errno.h>
 #include "Log.h"
 
-
 #define RIO_DUMMY false
 
-
 //TODO: different Errorhandling!
-void TCPServer::error(const char *funcName, const char *msg)
-{
-  ALL_LOG(logERROR) << "TCPServer::" << funcName << "(): " << msg;
+void TCPServer::error(const char *funcName, const char *msg) {
+	ALL_LOG(logERROR) << "TCPServer::" << funcName << "(): " << msg;
 }
 
 /*Opens TCP Server and establishes connection to a client(RoboRio)*/
 bool TCPServer::connect() {
-  if (RIO_DUMMY == false) {
-    if(clilen == 0) {
-      portno = 51717;
-      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-      if (sockfd < 0) {
-        error("connect","Error opening socket");
-        return false;
-      }
+	if (RIO_DUMMY == false) {
+		if (clilen == 0) {
+			portno = 51717;
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+			if (sockfd < 0) {
+				error("connect", "Error opening socket");
+				return false;
+			}
 
-      int opt = 1;
-      if (setsockopt( sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1 ) {
-        error("connect", "Error on setting Socket options");
-        return false;
-      }
+			int opt = 1;
+			if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+				error("connect", "Error on setting Socket options");
+				return false;
+			}
 
-      memset(&serv_addr, 0, sizeof(serv_addr));
-      serv_addr.sin_family = AF_INET;
-      serv_addr.sin_port = htons(portno);
-      serv_addr.sin_addr.s_addr = INADDR_ANY;
+			memset(&serv_addr, 0, sizeof(serv_addr));
+			serv_addr.sin_family = AF_INET;
+			serv_addr.sin_port = htons(portno);
+			serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-      if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        error("connect","Error on binding");
-        return false;
-      }
-      listen(sockfd,5);
-      clilen = sizeof(cli_addr);
-    }
+			if (bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+				error("connect", "Error on binding");
+				return false;
+			}
+			listen(sockfd, 5);
+			clilen = sizeof(cli_addr);
+		}
 
-    // Wait for a connection with a client
-    if ( ( newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t*) &clilen)) <0) {
-      error("connect","Error on accept");
-      return false;
-    }
-  }
-  return true;
+		// Wait for a connection with a client
+		if ((newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, (socklen_t*) &clilen)) < 0) {
+			error("connect", "Error on accept");
+			return false;
+		}
+	}
+	return true;
 }
 
 /*sends Message to Client (RoboRio). Takes Command and data packages as input*/
-bool TCPServer::sendTCP(int command, int eventVar, int data1, int data2, int data3) {  
-  if (RIO_DUMMY == false) {
-    ALL_LOG(logDEBUG4) << "TCPServer::sendTCP(): start";
-    int n; 
-    char buffer[COMMAND_LENGTH + (MessageLength)];
-    int m=sprintf( buffer, "%6d%6d%6d%6d%6d", command, eventVar, data1, data2, data3);
-    if (m != MessageLength) {
-      error("sendTCP","Error preparing message");
-      return false;
-    }
-    ALL_LOG(logDEBUG4) << "TCPServer::sendTCP(): Message prepared.";
-    if ( ( n = write( newsockfd, buffer, strlen(buffer) ) ) < 0 ) {
-      error("sendTCP","Error writing to socket");
-      return false;
-    }
-  }
-  ALL_LOG(logDEBUG4) << "TCPServer::sendTCP(" << command << ", "
-                                              << eventVar << ", "
-                                              << data1 << ", "
-                                              << data2 << ", "
-                                              << data3 << ")" ;
-  return true;
+bool TCPServer::sendTCP(int command, int eventVar, int data1, int data2, int data3) {
+	if (RIO_DUMMY == false) {
+		ALL_LOG(logDEBUG4) << "TCPServer::sendTCP(): start";
+		int n;
+		char buffer[COMMAND_LENGTH + (MessageLength)];
+		int m = sprintf(buffer, "%6d%6d%6d%6d%6d", command, eventVar, data1, data2, data3);
+		if (m != MessageLength) {
+			error("sendTCP", "Error preparing message");
+			return false;
+		}
+		ALL_LOG(logDEBUG4) << "TCPServer::sendTCP(): Message prepared.";
+		if ((n = write(newsockfd, buffer, strlen(buffer))) < 0) {
+			error("sendTCP", "Error writing to socket");
+			return false;
+		}
+	}
+	ALL_LOG(logDEBUG4) << "TCPServer::sendTCP(" << command << ", " << eventVar << ", " << data1 << ", " << data2 << ", " << data3 << ")";
+	return true;
 }
 
 /*reads Message from Client (RoboRio). Saves Command to commandRecieved
-and Data to dataRecieved[]*/
+ and Data to dataRecieved[]*/
 bool TCPServer::readTCP() {
-  if (RIO_DUMMY == false) {
-    memset(buffer, '\0', BUFFER_SIZE);
-    
-    char commandString[COMMAND_LENGTH];
-    char dataString[DATA_PACKAGES][DATA_LENGTH];
+	if (RIO_DUMMY == false) {
+		memset(buffer, '\0', BUFFER_SIZE);
 
-    int n= read(newsockfd, buffer, MessageLength);
+		char commandString[COMMAND_LENGTH];
+		char dataString[DATA_PACKAGES][DATA_LENGTH];
 
-    if(n==0 && NoDataCycleCount > 0) {
-      --NoDataCycleCount;
-      commandRecieved = 0;
-      return true;
-    }
-    else if (n==0 && NoDataCycleCount <= 0) {
-      error("readTCP","Connection Lost");
-      NoDataCycleCount = NOCONNECTION_COUNT;
-      return false;
-    }
-    else if (n != MessageLength) {
-      error("readTCP","Error reading from socket");
-      NoDataCycleCount = NOCONNECTION_COUNT;
-      return false;
-    }
+		int n = read(newsockfd, buffer, MessageLength);
 
-    memcpy(commandString, buffer, COMMAND_LENGTH);
-    commandRecieved = atoi(buffer);
-    for(int i = 0; i < DATA_PACKAGES; i++) {
-      memcpy(dataString[i], buffer + COMMAND_LENGTH + DATA_LENGTH*i, DATA_LENGTH);
-      dataRecieved[i] = atoi(dataString[i]);
-    }
-    NoDataCycleCount = NOCONNECTION_COUNT;
-  }
-  return true;
+		if (n == 0 && NoDataCycleCount > 0) {
+			--NoDataCycleCount;
+			commandRecieved = 0;
+			return true;
+		} else if (n == 0 && NoDataCycleCount <= 0) {
+			error("readTCP", "Connection Lost");
+			NoDataCycleCount = NOCONNECTION_COUNT;
+			return false;
+		} else if (n != MessageLength) {
+			error("readTCP", "Error reading from socket");
+			NoDataCycleCount = NOCONNECTION_COUNT;
+			return false;
+		}
+
+		memcpy(commandString, buffer, COMMAND_LENGTH);
+		commandRecieved = atoi(buffer);
+		for (int i = 0; i < DATA_PACKAGES; i++) {
+			memcpy(dataString[i], buffer + COMMAND_LENGTH + DATA_LENGTH * i, DATA_LENGTH);
+			dataRecieved[i] = atoi(dataString[i]);
+		}
+		NoDataCycleCount = NOCONNECTION_COUNT;
+	}
+	return true;
 }
-
 
 int TCPServer::getCommand() {
-  if ( readTCP() )
-    return commandRecieved;
-  else
-    return 99;
+	if (readTCP())
+		return commandRecieved;
+	else
+		return 99;
 }
 
-
 int TCPServer::getData(int dataPackage) {
-  if (dataPackage<0 || dataPackage > DATA_PACKAGES-1) {
-    ALL_LOG(logDEBUG4) << "no data[" << dataPackage << "] in communication protocol.";
-    return 0;
-  }
-  return dataRecieved[dataPackage];
+	if (dataPackage < 0 || dataPackage > DATA_PACKAGES - 1) {
+		ALL_LOG(logDEBUG4) << "no data[" << dataPackage << "] in communication protocol.";
+		return 0;
+	}
+	return dataRecieved[dataPackage];
 }
 
 int TCPServer::closeOnCommand(int command) {
-  close(newsockfd);
-  if ( command == -2)
-    return -1;
-  return 0;
-  
-}
+	close(newsockfd);
+	if (command == -2)
+		return -1;
+	return 0;
 
+}
 
