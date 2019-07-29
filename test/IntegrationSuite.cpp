7f2cdbdc3efdb2bf63f74d7c52bab297b7c7cd3c
Raphael Schroeder <raphael.schroeder@hsr.ch>
2019-07-29 10:37:16 +0200

    Basic Arm test

--- test/IntegrationSuite.cpp
+++ test/IntegrationSuite.cpp
@@ -4,47 +4,55 @@
 #include "Event.h"
 #include "EventIOFake.h"
 #include "RoboRioProtocol.h"
-
+#include "../include/LogFile.h"
 #include <thread>
 #include <vector>
+
+struct SequenceEvent {
+	std::chrono::milliseconds delay;
+	RoboRioProtocol::Packet command;
+};
+
+using namespace std::chrono_literals;
 
 struct EventsForBellSequence {
 	auto operator()() -> RoboRioProtocol::Packet {
 		if (nextCommandIndex >= commands.size()) {
-			FAILM("End of command sequence reached");
+			std::this_thread::sleep_for(100ms);
+			return RoboRioProtocol::Packet{KinovaFSM::Tick};
 		}
-		return commands.at(nextCommandIndex++);
+		auto event = commands.at(nextCommandIndex++);
+		std::this_thread::sleep_for(event.delay);
+		return event.command;
 	}
 private:
 	int nextCommandIndex{0};
-	std::vector<RoboRioProtocol::Packet> commands{
-		{KinovaFSM::Initialize},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Initialized},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Unfold},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Unfolded},
-		{KinovaFSM::GoToPosition, 2},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
-		{KinovaFSM::Tick},
+	std::vector<SequenceEvent> commands{
+		{1000ms, {KinovaFSM::Initialize}},
+		{3000ms, {KinovaFSM::Tick}},
+		{6000ms, {KinovaFSM::Initialized}},
+		{3000ms, {KinovaFSM::Tick}},
+		{7000ms, {KinovaFSM::Unfold}},
+		{3000ms, {KinovaFSM::Tick}},
+		{7000ms, {KinovaFSM::Unfolded}},
+		{1000ms, {KinovaFSM::GoToPosition, 4}},
 
 	};
 };
+template <typename EventFake>
+void tickUntilEnd(CommandHandling<EventFake> & commandHandling) {
+	for (int i = 0; i < 100; i++) {
+		commandHandling.process();
+	}
+}
 
 void thisIsAIntegrationSuiteTest() {
-	using namespace std::chrono_literals;
+	LogFile::create();
 	CommandHandling<EventIOFake<EventsForBellSequence>> commandHandling{};
-	for (int i = 0; i < 16; i++) {
+	for (int i = 0; i < 8; i++) {
 		commandHandling.process();
-		std::this_thread::sleep_for(1s);
 	}
+	tickUntilEnd(commandHandling);
 }
 
 cute::suite make_suite_IntegrationSuite() {
