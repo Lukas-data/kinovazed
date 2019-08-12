45e160e9f8c2c72fc7aa4dd4506fba2b0d01ea21
Thomas Corbat <tcorbat@hsr.ch>
2019-08-12 14:40:00 +0200

    Add non C++17 alternative to fold in streamArguments()

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -6,10 +6,26 @@
 
 namespace Kinova {
 
+
+#if __cplusplus >= 201703L
+//C++17
 template <typename...Args>
 void streamArguments(std::ostream & out, Args const &...args) {
 	(out << ... << args);
 }
+
+#else
+
+inline void streamArguments(std::ostream & out) {
+}
+
+template <typename First, typename...Args>
+void streamArguments(std::ostream & out, First const & first, Args const &...args) {
+	out << first;
+	streamArguments(out, args...);
+}
+
+#endif
 
 template <typename ExceptionType, typename...Args>
 auto composeException(Args const &...args) {
