a36031d70d6859f0d347687aec5294ab1fc671c6
Hansruedi Patzen <hpatzen@hsr.ch>
2019-08-21 14:51:18 +0200

    startup: use current script path

--- KinovaStartUp.sh
+++ KinovaStartUp.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-until mate-terminal -x bash -c 'sudo /home/kinova/Documents/kinovazed/build/src/KinovaZED -r;'; do
+SCRIPTPATH="$(cd "$(dirname "$0")" ; pwd -P)"
 
+sleep 10
+
+until mate-terminal -x bash -c 'cd ${SCRIPTPATH} && sudo ./KinovaZED -r;'; do
   echo "KinovaZED crashed with exit code $?. Restarting Script" >&2
   sleep 2
-
 done
 
