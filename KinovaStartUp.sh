1b8e73da9731831cf136a3385772c702ca115cab
Hansruedi Patzen <hpatzen@hsr.ch>
2019-08-21 15:00:28 +0200

    startup: create logging folders if needed

--- KinovaStartUp.sh
+++ KinovaStartUp.sh
@@ -2,8 +2,13 @@
 
 SCRIPTPATH="$(cd "$(dirname "$0")" ; pwd -P)"
 
+echo "Give the OS some time to finish booting"
 sleep 10
 
+echo "Creating logging folders if needed"
+mkdir -p ${SCRIPTPATH}/logfiles/archive
+
+echo "Starting KinovaZED"
 until mate-terminal -x bash -c 'cd ${SCRIPTPATH} && sudo ./KinovaZED -r;'; do
   echo "KinovaZED crashed with exit code $?. Restarting Script" >&2
   sleep 2
