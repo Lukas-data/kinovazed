6d581225e4b2282f3a10b4d3f67d3e7805ad1a9a
Hansruedi Patzen <hpatzen@hsr.ch>
2019-08-21 17:44:48 +0200

    KinovaMain: cleanup

--- src/KinovaMain.cpp
+++ src/KinovaMain.cpp
@@ -1,37 +1,24 @@
-
-/***************************************************************************
- *  Kinova Main
- *
- *  Created: Fri Jun 15 2018
- ****************************************************************************/
-
-
 #include "LogFile.h"
 #include "Log.h"
 #include "CommandHandling.h"
 
+#include <chrono>
 #include <stdexcept>
+#include <thread>
 
-
-
-
-
-int main(int argc, char *argv[])
-{
-  if ( LogFile::create() ) {
-    ALL_LOG(logINFO) << "KinovaMain - Startup!";
-    CommandHandling<> commandHandler{};
-
-    while (1) {
-      try {
-        commandHandler.process();
-      }
-      catch ( const std::runtime_error& e) {
-        ALL_LOG(logERROR) << "RuntimeError: " << e.what();
-        return -1;
-      }
-    }
-  }
-  usleep(10000000);
-  return -1;
+int main() {
+	if (LogFile::create()) {
+		ALL_LOG(logINFO) << "KinovaMain - Startup!";
+		CommandHandling<> commandHandler { };
+		while (true) {
+			try {
+				commandHandler.process();
+			} catch (std::runtime_error const &e) {
+				ALL_LOG(logERROR) << "RuntimeError: " << e.what();
+				return -1;
+			}
+		}
+	}
+	std::this_thread::sleep_for(std::chrono::seconds { 10 });
+	return -1;
 }
