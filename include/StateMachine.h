25b6cde8e94863a0052c536a1f615772e5f82cbe
Hansruedi Patzen <hpatzen@hsr.ch>
2019-08-21 13:30:05 +0200

    StateMachine: cleanup and formatting

--- include/StateMachine.h
+++ include/StateMachine.h
@@ -8,7 +8,6 @@
 
 #include <memory>
 
-
 /*This State Machine runs the entry, exit and tick events from the states in "State.h" according to the events and transitions in "Transition.h". There is no Queue. If an event finds no use, it is dropped.*/
 
 constexpr auto looptime = 50.0;
@@ -16,16 +15,13 @@ constexpr auto looptime = 50.0;
 struct StateMachine {
 	explicit StateMachine(std::shared_ptr<KinovaArm> jacoZed);
 
-    //Send Event to the StateMachine.
-    void sendEvent(KinovaFSM::Event e, int eventVar = 0);
+	//Run the StateMachine. Return True if StateChange is performed.
+	bool process(KinovaFSM::Event e, int var);
 
-    //Run the StateMachine. Return True if StateChange is performed.
-    bool process(KinovaFSM::Event e, int var);
-
-  private:
-    State* CurrentState = nullptr;
-    int NumberOfTransitions = sizeof(KinovaFSM::TransitionTable)/sizeof(KinovaFSM::Transition);
-    timespec LastTick;
+private:
+	State *CurrentState;
+	int NumberOfTransitions;
+	timespec LastTick;
 };
 
 #endif
--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -5,39 +5,33 @@
 #include <ctime>
 #include <memory>
 
-
-StateMachine::StateMachine(std::shared_ptr<KinovaArm> jacoZed) : CurrentState{KinovaFSM::initState} {
+StateMachine::StateMachine(std::shared_ptr<KinovaArm> jacoZed) :
+		CurrentState { KinovaFSM::initState }, NumberOfTransitions { sizeof(KinovaFSM::TransitionTable) / sizeof(KinovaFSM::Transition) } {
 	CurrentState->init(jacoZed);
 	clock_gettime(CLOCK_REALTIME, &LastTick);
 }
 
-/*Processes InputEvent and InputVariable in Statemachine. Returns true if state Change is peformed.*/
 bool StateMachine::process(KinovaFSM::Event e, int var) {
-  
-  //Print handled Event and reset InputEvent.
-  for (int i = 0; i<NumberOfTransitions; i++) {
-    if ( (CurrentState == KinovaFSM::TransitionTable[i].currentState) && (e == KinovaFSM::TransitionTable[i].event) )  {
-      ALL_LOG(logINFO) << "StateMachine: Processing Event '" << KinovaFSM::eventNames[e] << "'";
-      KinovaFSM::TransitionTable[i].currentState->exitAction();
-      CurrentState = KinovaFSM::TransitionTable[i].nextState;
-      CurrentState->setEventVar(var);
-      CurrentState->entryAction();
-      return true;
-    }      
-  }
-  ALL_LOG(logDEBUG4) << "StateMachine: Not Processing Event '" << KinovaFSM::eventNames[e] << "'";
-  timespec timeNow;
-  clock_gettime(CLOCK_REALTIME, &timeNow);
-  double elapsedTime = (timeNow.tv_sec-LastTick.tv_sec) * 1000.0 +
-                    (timeNow.tv_nsec-LastTick.tv_nsec) / 1000000.0;
-  if( elapsedTime > looptime ) {
-    ALL_LOG(logDEBUG4) << "Tick!";
-    CurrentState->tickAction();
-    clock_gettime(CLOCK_REALTIME, &LastTick);
-  }
-  
-  return false;
+	for (int i = 0; i < NumberOfTransitions; i++) {
+		if ((CurrentState == KinovaFSM::TransitionTable[i].currentState) && (e == KinovaFSM::TransitionTable[i].event)) {
+			ALL_LOG(logINFO) << "StateMachine: Processing Event '" << KinovaFSM::eventNames[e] << "'";
+			KinovaFSM::TransitionTable[i].currentState->exitAction();
+			CurrentState = KinovaFSM::TransitionTable[i].nextState;
+			CurrentState->setEventVar(var);
+			CurrentState->entryAction();
+			return true;
+		}
+	}
+
+	ALL_LOG(logDEBUG4) << "StateMachine: Not Processing Event '" << KinovaFSM::eventNames[e] << "'";
+	timespec timeNow;
+	clock_gettime(CLOCK_REALTIME, &timeNow);
+	double elapsedTime = (timeNow.tv_sec - LastTick.tv_sec) * 1000.0 + (timeNow.tv_nsec - LastTick.tv_nsec) / 1000000.0;
+	if (elapsedTime > looptime) {
+		ALL_LOG(logDEBUG4) << "Tick!";
+		CurrentState->tickAction();
+		clock_gettime(CLOCK_REALTIME, &LastTick);
+	}
+	return false;
 }
-
-
 
