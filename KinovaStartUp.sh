5695a899535c02619697c2f1339b76b8256bca6f
Hansruedi Patzen <hpatzen@hsr.ch>
2019-08-22 13:31:57 +0200

    startup: export script path

--- KinovaStartUp.sh
+++ KinovaStartUp.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH="$(cd "$(dirname "$0")" ; pwd -P)"
+KINOVABUILD="$(cd "$(dirname "$0")" ; pwd -P)"
 LASTLOG=/tmp/KinovaStartUp.log
 
 if [ -f ${LASTLOG} ]; then
   rm -f ${LASTLOG}
 fi
 
-echo "Scriptpath: ${SCRIPTPATH}" >> ${LASTLOG}
+echo "Kinova build path: ${KINOVABUILD}" >> ${LASTLOG}
 
 echo "Give the OS some time to finish booting" >> ${LASTLOG}
 sleep 10
 
 echo "Creating logging folders if needed" >> ${LASTLOG}
-mkdir -p ${SCRIPTPATH}/logfiles/archive
+mkdir -p ${KINOVABUILD}/logfiles/archive
+
+export KINOVABUILD
 
 echo "Starting KinovaZED" >> ${LASTLOG}
-until mate-terminal -x $(cd ${SCRIPTPATH} && sudo ./KinovaZED); do
+until mate-terminal -x bash -c 'cd ${KINOVABUILD} && sudo ./KinovaZED'; do
   echo "KinovaZED crashed with exit code $?. Restarting Script" >> ${LASTLOG}
   sleep 2
 done
