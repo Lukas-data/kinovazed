a16f28dac8e1ccc9bbc24fede2227cd193b7a76e
Hansruedi Patzen <hpatzen@hsr.ch>
2019-08-28 13:09:43 +0200

    statemachine: remove last tick time logging

--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -27,7 +27,6 @@ bool StateMachine::process(KinovaFSM::Event e, int var) {
 
 	ALL_LOG(logDEBUG4) << "StateMachine: Not Processing Event '" << KinovaFSM::eventNames[e] << "'";
 	auto elapsedTime { std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - lastTick) };
-	ALL_LOG(logDEBUG4) << "StateMachine: Time since last tick " << elapsedTime.count() << "ms";
 	if (elapsedTime > 50ms) {
 		ALL_LOG(logDEBUG4) << "Tick!";
 		currentState->tickAction();
