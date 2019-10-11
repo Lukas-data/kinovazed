ef6f73545c2ede6d55bc0a7b3e37b23e902c2fcd
smiracco <sergio.miracco@gmail.com>
2019-10-11 15:34:25 +0200

    log: replace in main for compile check

--- src/KinovaMain.cpp
+++ src/KinovaMain.cpp
@@ -1,5 +1,4 @@
-#include "LogFile.h"
-#include "Log.h"
+
 #include "CommandHandling.h"
 
 #include <chrono>
@@ -9,19 +8,21 @@
 
 int main() {
 	if (LogFile::create()) {
-		ALL_LOG(logINFO) << "KinovaMain - Startup!";
+		spdlog::info("KinovaMain - Startup!");
+		//ALL_LOG(logINFO) << "KinovaMain - Startup!";
 		CommandHandling<> commandHandler { };
 		while (true) {
 			try {
 				commandHandler.process();
 			} catch (std::runtime_error const &e) {
-				ALL_LOG(logERROR) << "RuntimeError: " << e.what();
+				spdlog::error("RuntimeError: {}", e.what());
+				//ALL_LOG(logERROR) << "RuntimeError: " << e.what();
 				return -1;
 			} catch (std::exception const &e) {
-				ALL_LOG(logERROR) << "Exception: " << e.what();
+				//ALL_LOG(logERROR) << "Exception: " << e.what();
 				return -1;
 			} catch (...) {
-				ALL_LOG(logERROR) << "UNKNOWN ERROR.";
+				//ALL_LOG(logERROR) << "UNKNOWN ERROR.";
 				return -1;
 			}
 		}
